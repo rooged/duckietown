95cbc9915fd2dd23bbeef2a70a49a8c0d696e4a3
rooged <roo_ged65@yahoo.com>
2020-11-11 22:12:51 -0500

    Update

---
+++ packages/homework4/src/subscribe.py
@@ -0,0 +1,17 @@
+#!/usr/bin/envpython
+
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+def listener():
+	rospy.init_node('subscribe', anonymous=True)
+	
+	rospy.Subscriber("delta", String, callback)
+	
+	rospy.spin()
+		
+if __name__ == '__main__':
+	listener()
