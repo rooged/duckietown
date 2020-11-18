28ede914a47d85a5a6749df57b6ef682137a3aea
rooged <roo_ged65@yahoo.com>
2020-11-18 18:47:46 -0500

    project3

--- packages/project3/src/project3.py
+++ packages/project3/src/project3.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 import rospy
+from duckietown_msgs.msg import Twist2DStamped
 from std_msgs.msg import Float32
 
 def talker():
 	pub = rospy.Publisher("/duck69/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
 	rospy.init_node('project3', anonymous=True)
 	rate = rospy.Rate(10) #10 Hz
-	velocity = 0.4099999964237213
-	omega = 8.300000190734863
+	velocity = 0.3
+	o = 2.5
 	while not rospy.is_shutdown():
-		pub.publish(Twist2DStamped(header=None, v=velocity, o=omega))
+		pub.publish(Twist2DStamped(header=None, v=velocity, omega=o))
 		rate.sleep()
 		
 if __name__ == '__main__':
