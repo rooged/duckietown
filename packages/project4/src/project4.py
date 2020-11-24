51b4ccc16924ff44a7c06aa2e0c3a4b91271776a
rooged <roo_ged65@yahoo.com>
2020-11-24 02:03:07 -0500

    project 4

--- packages/project4/src/project4.py
+++ packages/project4/src/project4.py
@@ -5,18 +5,18 @@ import math
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import Pose2DStamped
 from std_msgs.msg import Float32
+from std_msgs.msg import String
 
 global phi
-phi = None
+phi = 0
 
 class robot:	
 	def __init__(self):
-		self.phi_previous_error = 0
-		self.d_previous_error = 0
+		self.previous_error = 0
 		self.set_point = 1
-		self.kp = 1
+		self.kp = 1.5
 		self.ki = 0
-		self.kd = 0
+		self.kd = 4
 		self.integral = 0
 		self.dt = .1
 		
@@ -26,30 +26,24 @@ class robot:
 		self.previous_error = self.error
 
 		return self.kp * self.error + self.kd * self.derivative
-		
-class talker:
-	def callback(data):
-		value = data
 
-	def __init__(self):
-		self.sub = rospy.Subscriber("/duck69/lane_controller_node/lane_post", Pose2DStamped, self.callback)
-		self.pub = rospy.Publisher("/duck69/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+def callback(data):
+	phi = data.data
 
-	def talk():
-		r = robot()
-		velocity = .3
+def talk():
+	rospy.init_node('project4', anonymous=True)
+	rate = rospy.Rate(10) #10 Hz
+	pub = rospy.Publisher("/duck69/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+	r = robot()
+	while not rospy.is_shutdown():
+		rospy.Subscriber("/duck69/lane_controller_node/lane_post", Pose2DStamped, callback)
+		rospy.loginfo("Timothy Gedney lane following node")
 		o = r.pid(phi)
-		pub.publish(Twist2DStamped(header=None, v=velocity, omega=o))
+		pub.publish(Twist2DStamped(header=None, v=.15, omega=o))
+		rate.sleep()
 		
 if __name__ == '__main__':
 	try:
-		t = start()
-		rospy.init_node('project4', anonymous=True)
-		rate = rospy.Rate(10) #10 Hz
-		while not rospy.is_shutdown():
-			rospy.loginfo("Timothy Gedney lane following node")
-			t.talk()
-			rate.sleep()
-			rospy.spin()
+		talk()
 	except rospy.ROSInterruptException:
 		pass
